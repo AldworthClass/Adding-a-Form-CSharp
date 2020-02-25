00a9b75724cd53e67268f24b55d270bb603d80c9
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-02-25 11:51:57 -0500

    Fixed adding empty name

--- Adding a Form/FormAddName.cs
+++ Adding a Form/FormAddName.cs
@@ -24,17 +24,19 @@ namespace Adding_a_Form
         private void btnAddName_Click(object sender, EventArgs e)
         {
             if (FormMain.names.Contains(txtAddName.Text.Trim()))
-                MessageBox.Show("Name already exists");
-            else
+                MessageBox.Show("Name already exists!");
+            else if (txtAddName.Text.Trim().Length == 0)
             {
+                MessageBox.Show("Please type a name!");
+            }
+            else
+            { 
                 FormMain.names.Add(txtAddName.Text.Trim());
                 FormMain.names.Sort();
                 lstNames.DataSource = null;
                 lstNames.DataSource = FormMain.names;
             }
-            
         }
-
         private void FormAddName_Load(object sender, EventArgs e)
         {
             lstNames.DataSource = FormMain.names;   //Fills ListBox with names
